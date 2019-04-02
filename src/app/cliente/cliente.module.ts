cf9102924f86a5e106718d203339d273007d0940
aa-rodriguezv <angelrodri390@hotmail.com>
2019-04-02 06:07:44 -0500

    1 file modified

--- src/app/cliente/cliente.module.ts
+++ src/app/cliente/cliente.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {ClienteListComponent} from './cliente-list/cliente-list.component';
+import {ClienteService} from './cliente.service';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
-  declarations: []
+  declarations: [ClienteListComponent],
+  providers: [ClienteService],
+  exports: [ClienteListComponent]
 })
-export class clienteModule { }
+export class ClienteModule { }
