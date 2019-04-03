36ad4fb837a3c04c8f4652a04e9a0f71c282d8e5
aa-rodriguezv <angelrodri390@hotmail.com>
2019-04-03 14:23:39 -0500

    2 files modified

--- src/app/cliente/cliente-create/cliente-create.component.ts
+++ src/app/cliente/cliente-create/cliente-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { ClienteService } from '../cliente.service';
 
 @Component({
   selector: 'app-cliente-create',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private clienteService: ClienteService,
+    private toastrService: ToastrService
+  ) { }
 
   ngOnInit() {
   }
